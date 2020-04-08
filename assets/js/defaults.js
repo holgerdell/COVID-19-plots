c20e66d05bc0755f77a3faed12d45de24acef642
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-04-08 10:23:35 +0200

    refactor: Move defaults to own file.

---
+++ assets/js/defaults.js
@@ -0,0 +1,30 @@
+import * as data from './data.js'
+
+const defaultCountries = [
+  'China', 'Italy', 'Denmark', 'Germany', 'Sweden', 'Greece', 'France'
+]
+
+/* This dictionary holds the default values for the state
+ * new toggles and options can simply be added here */
+export const state = {
+  plot: 'calendar',
+  colorScheme: 'light',
+  dataset: data.defaultDataset,
+  countries: defaultCountries,
+  params: {
+    calendar: {
+      align: false,
+      cumulative: true,
+      normalize: true,
+      logplot: true,
+      smooth: false
+    },
+    trajectory: {
+      logplot: true,
+      smooth: true
+    },
+    doubling: {
+      smooth: true
+    }
+  }
+}
